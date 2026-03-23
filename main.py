38d57e3d1267ae0bde976234328d209efd9c3848
Alisson Casagrande <contato@grande.app.br>
2026-03-23 16:33:27 -0300

    feat: update main.py with latest CLI logic

--- main.py
+++ main.py
@@ -55,12 +55,18 @@ def print_main_menu():
     return key
 
 def fill_fields(task=None):
+    allowed_status = ['todo', 'in_progress', 'done']
+
     print("") #just jump 1 line
     title = input(f"New Title [leave blank to {'keep' if task else 'default'}]: ")
     description = input(f"New Description [leave blank to {'keep' if task else 'default'}]: ")
 
     print('Status must be one of: "todo", "in_progress", "done"')
     status = input(f"New Status [leave blank to {'keep' if task else 'default to todo'}]: ").lower()
+
+    if status and (status not in allowed_status): #check if status is not None and is allowed
+        print(f"Invalid status: {status}. Must be one of {allowed_status}.")
+        return None
 
     if task:
         # UPDATE: altera atributos da instância existente
@@ -74,27 +80,51 @@ def fill_fields(task=None):
         description = description if description else ""
         status = status if status else "todo"
         tt = Task(title=title, description=description, status=status)
-
     return tt
 
 def delete_task(conn, task):
-    tt = task.delete_task(conn)
+    if (task is not None):
+        try:
+            return task.delete_task(conn)
+        except sqlite3.IntegrityError as e:
+            print("Erro de integridade:", e)
+            return None
+        except sqlite3.Error as e:
+            print("Erro geral do SQLite:", e)
+            return None
+    return None
 
 def update_task(conn, task):
     ff = fill_fields(task) #returns a ff(Task) updated
-    tt = ff.update_task(conn) #update database, parameters will not be necessary, filds are ok on ff.
-    return tt
+    if (ff is not None):
+        try:
+            return ff.update_task(conn) #update database, parameters will not be necessary, filds are ok on ff.
+        except sqlite3.IntegrityError as e:
+            print("Erro de integridade:", e)
+            return None
+        except sqlite3.Error as e:
+            print("Erro geral do SQLite:", e)
+            return None
+    return None
 
 def add_task(conn):
     ff = fill_fields(None) #create a new ff(Task) with id = None
-    tt = Task.create_task(conn, ff.title, ff.description, ff.status) #insert database, return tt (with id)
-    return tt
+    if (ff is not None):
+        try:
+            return Task.create_task(conn, ff.title, ff.description, ff.status) #insert database, return tt (with id)
+        except sqlite3.IntegrityError as e:
+            print("Erro de integridade:", e)
+            return None
+        except sqlite3.Error as e:
+            print("Erro geral do SQLite:", e)
+            return None
+    return None
 
 def print_view_task(connection, task_id, task_list=None):
     tt = None
-    for task_id in task_list:
-        if task_id == task_id:
-            tt = task_id
+    for x in task_list:
+        if task_id == x.id:
+            tt = x
             break
     
     if tt is None:     
@@ -122,11 +152,17 @@ def print_view_task(connection, task_id, task_list=None):
             elif (key == '1'):
                 print("Updating Task...")
                 tt = update_task(connection,tt)
+                input("Task updated successfully! [press Enter]")
+                if (tt is None):
+                    input("Error updating task...[press Enter]")
                 break
             elif (key == '2'):
-                a = input("It will delete this task. Are you sure? (Y/N): ")
+                a = input("It will delete this task. Are you sure? (Y/N): ")               
                 if (a.upper() == 'Y'):
                     tt = delete_task(connection, tt)
+                    input("Task deleted successfully! [press Enter]")
+                    if (tt is None):
+                        input("Error deleting task...[press Enter]")
                     break
             else:
                 print(f"Invalid input: {key}. Please press * to exit.")
@@ -169,10 +205,11 @@ def main_loop():
             allTasks = list_all_tasks(connection)
             print("--------------------------------------------------------------")
             if allTasks is None or len(allTasks) == 0:
-                input ("Press any key to return to the main menu")
+                input ("Press [Enter] key to return to the main menu")
             else:
                 while True:
-                    c = input("Press * to return to the main menu, or ID of the task to view details.")
+                    print("Press * to return to the main menu, or")
+                    c = input("Inform the ID of the task to view details: ")
                     if (c == '*'): break
                     if c != '*' and c.isdigit() and len(allTasks) > 0:
                         print_view_task(connection, int(c), allTasks)
@@ -180,6 +217,10 @@ def main_loop():
         elif key == '2':
             print("Adding a new task...")
             t = add_task(connection)
+            if (t is None):
+                input("Error adding task...[press Enter]")
+            else:
+                input(f"Task {t.id} added successfully!  [press Enter]")
 
         # just to see what is happening
         print(f"You pressed: {key}. Processing input...")
